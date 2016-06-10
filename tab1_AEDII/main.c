dfc763b6bfc211ba9f12cd4a4f360a02f2338b66
Caio Arthur <csalestelles@gmail.com>
2016-06-10 17:45:58 -0400

    Functions of insertion and print

--- tab1_AEDII/main.c
+++ tab1_AEDII/main.c
@@ -7,9 +7,95 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[]) {
+typedef struct noh{
+    int key;
+    struct noh *left;
+    struct noh *right;
+}noh;
+
+int selectedOption;
+noh *mainRoot;
+
+int Menu(void);
+void insert(noh *node, noh *root), selectOption(int value);
+
+int main()
+{
+    selectedOption = Menu();
+    selectOption(selectedOption);
+    main();
+}
+
+noh *createNode(int value)
+{
+    noh *newNode;
+    newNode = (noh *)malloc(sizeof(noh));
     
-    printf("Hello, World, caio!\n");
-    return 0;
+    newNode->key = value;
+    newNode->right = NULL;
+    newNode->left = NULL;
+    
+    return newNode;
+}
+
+void insertOut()
+{
+    int valuetoBeInserted;
+    
+    printf("Número à ser inserido: ");
+    scanf("%d", &valuetoBeInserted);
+    insert(createNode(valuetoBeInserted), mainRoot);
+    
+}
+
+void insert(noh *node, noh *root)
+{
+    if (root == NULL)
+    {
+        root = node;
+    }
+    else
+    {
+        if (node->key < (root)->key)
+        {
+            insert(node, (root)->left);
+        }
+        else if (node->key > (root)->key)
+        {
+            insert(node, (root)->right);
+        }
+    }
+}
+
+void showValues(noh *root)
+{
+    if (root != NULL)
+    {
+        printf("%d, ", (root)->key);
+        showValues(root->left);
+        showValues(root->right);
+    }
+}
+
+int Menu(void)
+{
+    int option;
+    printf("Inserir na árvore - 1\n\nExibir - 0\nOpção: ");
+    scanf("%d", &option);
+    return option;
+}
+
+
+void selectOption(int value)
+{
+   if (value == 1)
+   {
+       insertOut();
+   }
+   else if (value == 0)
+   {
+       showValues(mainRoot);
+   }
 }
